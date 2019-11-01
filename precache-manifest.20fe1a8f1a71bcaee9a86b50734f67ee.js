a83895b262ed796906d096ee6f3a171b45254421
Shariar Shaikot <shariar.shaikot@frontmen.nl>
2019-11-01 16:19:27 +0100

    add build version to github pages

---
+++ precache-manifest.20fe1a8f1a71bcaee9a86b50734f67ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edeabf427dc874658c734c21a4851105",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9a457580f8fa253e1f6",
+    "url": "/static/css/main.6cc523a1.chunk.css"
+  },
+  {
+    "revision": "b1c502987e5cdd157f8f",
+    "url": "/static/js/2.acce9bd2.chunk.js"
+  },
+  {
+    "revision": "e9a457580f8fa253e1f6",
+    "url": "/static/js/main.59635244.chunk.js"
+  },
+  {
+    "revision": "adc3bc36bdde6e38b6de",
+    "url": "/static/js/runtime-main.a84674db.js"
+  }
+]);
--- src/cards/index.js
+++
@@ -1,5 +0,0 @@
-import Home from "./Home"
-import Share from "./Share"
-import Download from "./Download"
-
-export { Home, Share, Download }
